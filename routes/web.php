bc0ae84ba80b5fb973a1ecf8687898a5ef800f57
nachat-ayoub <venomx2020x@gmail.com>
2023-05-11 02:06:58 +0100

    upgrade to laravel v10 & add auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\FormateurController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,84 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
- */
-
-Route::group(['prefix' => 'absence'], function () {
-    Route::get('/{absence_id}',
-        [FormateurController::class, 'getStagiaire']
-    );
-
-    // * Creation :
-    Route::get('/{absence_id}/stagiaire/{stagiaire_id}',
-        [FormateurController::class, 'createAbsenceStagiaire']
-    );
-
-    Route::post('/{absence_id}/stagiaire/{stagiaire_id}',
-        [FormateurController::class, 'storeAbsenceStagiaire']
-    );
-
-    // * Update :
-    Route::get('/{absence_id}/stagiaire/{stagiaire_id}',
-        [FormateurController::class, 'editAbsenceStagiaire']
-    );
-
-    Route::put('/{absence_id}/stagiaire/{stagiaire_id}',
-        [FormateurController::class, 'updateAbsenceStagiaire']
-    );
-
-    // * Delete :
-    Route::delete('/{absence_id}/stagiaire/{stagiaire_id}',
-        [FormateurController::class, 'destroyAbsenceStagiaire']
-    );
-});
-
-/*
+*/
 
 Route::get('/', function () {
-$admin = App\Models\Admin::find(1); // badr
-return $admin->formateurs;
+    return view('welcome');
 });
 
-Route::get('/classes', function () {
-$admin = App\Models\Admin::find(1); // badr
-return $admin->classes;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/stagiaire', function () {
-$class = App\Models\Classe::find(1);
-return $class->stagiaires;
-});
-
-Route::get('/absence', function () {
-$abs = App\Models\Absence::find(1);
-return $abs->formateur;
-});
-
-Route::get('/absence_stg', function () {
-$prv = App\Models\Absence_stagiaire::find(2);
-return "<p>" . $prv->stagiaire->nom . "   " . $prv->absence->date . "</p>";
-});
-
-Route::get('formateurs', [AdminController::class, 'indexFormateur']);
-
- */
-
-// Route::get('formateurs', [AdminController::class , 'indexFormateur']);
-// Route::get('formateurs/store', [AdminController::class , 'storeFormateur']);
-// Route::get('formateurs/update', [AdminController::class , 'updateFormateur']);
-// Route::delete('formateurs/delete', [AdminController::class , 'destroyFormateur']);
-
-// Route::get('stagiaires', [AdminController::class , 'indexStagiaire']);
-// Route::get('stagiaires/store', [AdminController::class , 'storeStagiaire']);
-// Route::get('stagiaires/update', [AdminController::class , 'updateStagiaire']);
-// Route::delete('stagiaires/delete', [AdminController::class , 'destroyStagiaire']);
-// Route::get('stagiaires/branchStagiaire', [AdminController::class , 'searchStagiaire']);
-
-// Route::get('classes', [AdminController::class , 'indexClasses']);
-// Route::get('classes/store', [AdminController::class , 'storeClasse']);
-// Route::get('classes/update', [AdminController::class , 'updateClasse']);
-// Route::get('stagiaires/delete', [AdminController::class , 'destroyClasse']);
-// Route::get('claasseStagiaires', [ AdminController::class , 'allStagiaires' ] );
+require __DIR__.'/auth.php';
