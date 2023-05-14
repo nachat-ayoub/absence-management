d7a81f32a8a8231218c44e3aec4e9a838448f33c
nachat-ayoub <venomx2020x@gmail.com>
2023-05-14 21:04:55 +0100

    add nav dropdowns

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -58,6 +58,79 @@
                                 </x-slot>
                             </x-dropdown>
                         </div>
+
+
+                        {{-- Stagiaire --}}
+                        <div class="">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button
+                                        class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300">
+                                        <div>
+                                            Stagiaire
+                                        </div>
+
+                                        <div class="ml-1">
+                                            <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    {{-- <x-dropdown-link href="route('admin.searchStagiaire')"> --}}
+                                    <x-dropdown-link :href="'/admin/stagiaire/search'">
+                                        {{ __('Chercher') }}
+                                    </x-dropdown-link>
+
+                                    {{-- <x-dropdown-link href="route('admin.createStagiaire')"> --}}
+                                    <x-dropdown-link :href="'/admin/stagiaire/create'">
+                                        {{ __('Creé') }}
+                                    </x-dropdown-link>
+
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+
+                        {{-- Classe --}}
+                        <div class="">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button
+                                        class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300">
+                                        <div>
+                                            Classe
+                                        </div>
+
+                                        <div class="ml-1">
+                                            <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    {{-- <x-dropdown-link href="route('admin.searchClasse')"> --}}
+                                    <x-dropdown-link :href="'/admin/classe/search'">
+                                        {{ __('Chercher') }}
+                                    </x-dropdown-link>
+
+                                    {{-- <x-dropdown-link href="route('admin.createClasse')"> --}}
+                                    <x-dropdown-link :href="'/admin/classe/create'">
+                                        {{ __('Creé') }}
+                                    </x-dropdown-link>
+
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
                     @endauth
 
                     {{-- Profile --}}
