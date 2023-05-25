e4ae2b19f4b70cf9cc20b1910bc1a69f0ba0285c
mustapha122 <qepucogime@rungel.net>
2023-05-25 19:42:15 +0100

    update factory of PresenceFactory

--- database/factories/PresenceFactory.php
+++ database/factories/PresenceFactory.php
@@ -20,7 +20,6 @@ class PresenceFactory extends Factory
             'stagiaire_id' => random_int(1, 10),
             'classe_id' => 1,
             'date' => fake()->dateTimeBetween("this week Monday", "now"),
-            'seance' => fake()->randomElement(['8:30 11:00', '11:00 13:20', '13:30 16:00', '16:00 18:30', '8:30 13:20', '13:30 18:30']),
             'isPresence' => fake()->boolean(),
             'preuve' => fake()->randomElement(['rien', 'medicale']),
         ];
