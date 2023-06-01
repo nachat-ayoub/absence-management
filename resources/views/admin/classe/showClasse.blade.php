25a4e74b3a629949ecbf771908fd14e04ea93813
Abodo_Hatim <hatimgit2002@gmail.com>
2023-06-02 00:04:15 +0100

    fix padding style in showClasse

--- resources/views/admin/classe/showClasse.blade.php
+++ resources/views/admin/classe/showClasse.blade.php
@@ -4,7 +4,7 @@
             {{ __("classe $classe->id") }}
         </h2>
     </x-slot>
-    <div class="container mx-auto  mt-10 px-4">
+    <div class="container mx-auto  py-10 px-4">
         <button class='inline-flex items-center justify-center px-1 py-2 md:px-3 md:py-2 mb-4  bg-gray-800 dark:text-gray-200 dark:border-gray-400 border border-transparent rounded-lg text-white tracking-widest hover:text-gray-100 dark:hover:text-gray-800 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150'>
             <a href="{{route('admin.allClasses')}}">Retourner</a>
         </button>
